c97b01f613be496337c0b7e9e9ccc46c818865d4
deechris27 <deechris27@dee.com>
2018-04-17 17:26:38 +0530

    more ways

--- reverse.js
+++ reverse.js
@@ -31,6 +31,30 @@ function revString(str){
   }
 }
 
+function reverseStr(str){
+  let revString = '';
+  str.split('').forEach(function(char){
+     revString = char + revString;
+  });
+  return revString;
+}
+
+  function reverseStr(str){
+    let revString = '';
+    str.split('').forEach(e => revString = e + revString);
+    return revString;
+  }
+
+  function reverseStr(str){
+    return str.split('').reduce(function(revString, char){
+      return char + revString;
+    }, '');
+  }
+
+  function reverseStr(str){
+    return str.split('').reduce((revString, char)=> char + revString, '');
+  }
+
 
 
 console.log(reverseStr('helloo'));
